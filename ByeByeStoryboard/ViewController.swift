919ccbee965c4f52ef9f67186d83cfc280330a72
Lukas Gerhardt <lukas.gerhardt@tum.de>
2019-08-02 12:12:07 +0200

    Add Xcode project to repo

---
+++ ByeByeStoryboard/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ByeByeStoryboard
+//
+//  Created by Lukas Gerhardt on 02.08.19.
+//  Copyright © 2019 Lukas Gerhardt. All rights reserved.
+//
+
+// MARK: Imports
+import UIKit
+
+// MARK: - MainViewController
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
